4dab71e9fdf69fed0be363af72f8233477cc650f
chaixshot <chai5142xshot@gmail.com>
2026-04-20 19:10:11 +0700

    enable start button after storage permission granted

--- app/src/main/java/com/hamer/pico2dock/PermissionHelper.java
+++ app/src/main/java/com/hamer/pico2dock/PermissionHelper.java
@@ -3,6 +3,7 @@ package com.hamer.pico2dock;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.widget.Button;
 
 import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
@@ -10,9 +11,11 @@ import androidx.core.content.ContextCompat;
 
 public class PermissionHelper {
     static CardView WarningCard;
+    static Button ButtonStart;
 
     public static void CheckPermission(Activity context) {
         WarningCard = (CardView) context.findViewById(R.id.CardPermissionWarning);
+        ButtonStart = (Button) context.findViewById(R.id.ButtonStart);
 
         if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             WarningCard.setVisibility(CardView.VISIBLE);
@@ -26,5 +29,6 @@ public class PermissionHelper {
 
     public static void PermissionsGranted() {
         WarningCard.setVisibility(CardView.GONE);
+        ButtonStart.setEnabled(true);
     }
 }
